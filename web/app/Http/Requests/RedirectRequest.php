a99f1a0cdf1399904759598b6bbcbf7f00ebeff1
Rodrigo <rpanes@tuxpan.com>
2024-03-22 16:04:49 -0300

    validacion QA redirect

--- web/app/Http/Requests/RedirectRequest.php
+++ web/app/Http/Requests/RedirectRequest.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Http\Requests;
-
-use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\NotNull;
 
 class RedirectRequest extends CustomFormRequest
 {
@@ -11,28 +10,22 @@ class RedirectRequest extends CustomFormRequest
     {
         return [
             'IdCarro' => 'required|numeric|digits_between:1,20',
-            'CodRet' => 'required',
-            'Estado' => 'required',
-            'mpfin' => 'required'
-        ];
-
-        /*  
-            'IdCarro' => 'required|numeric|digits_between:1,20',
-            'CodRet' => 'required|string|in:001,002,003',
+            'CodRet' => 'required|string|in:000,001,002',
             'Estado' => 'required|string|in:ACEPTADO,RECHAZADO,PENDIENTE',
             'mpfin' => 'required|array',
-            'mpfin.*.IDTRX' => 'required|numeric|digits_between:1,20',
-            'mpfin.*.CODRET' => 'required|string|in:001,002,003',
-            'mpfin.*.NROPAGOS' => 'required|numeric|min:1|max:99',
-            'mpfin.*.TOTAL' => 'required|numeric|digits_between:1,18',
-            'mpfin.*.INDPAGO' => 'required|string|in:S,N',
-            'mpfin.*.IDREG' => 'required|numeric|digits_between:1,20'
-        */
+            'mpfin.IDTRX' => 'required|numeric|digits_between:1,20',
+            'mpfin.CODRET' => 'required|string|in:00,001,002',
+            'mpfin.NROPAGOS' => 'required|numeric|min:1|max:99',
+            'mpfin.TOTAL' => 'required|numeric|digits_between:1,18',
+            'mpfin.INDPAGO' => 'required|string|in:S,N',
+            'mpfin.IDREG' => 'required|numeric|digits_between:1,20'
+        ];
     }
 
     public function prepareForValidation()
     {
-        $mpfinXml = $this->input('mpfin');
+        
+        $mpfinXml = (!empty($this->input('mpfin')) && $this->input('mpfin') != 'null') ? $this->input('mpfin') : '';
         $mpfinArray = json_decode(json_encode(simplexml_load_string($mpfinXml)), true);
         $this->merge(['mpfin' => $mpfinArray]);
     }
